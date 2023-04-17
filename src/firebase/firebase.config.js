e4d55f459285deeb33de0db169d16c990fd1c51c
ashraful amin <ashrafulamin654@gmail.com>
2023-04-17 14:52:53 +0600

    60-1 Module Introduction and Basic Project Setup

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCBsxBLRTSDfp38YExRdCxMi5XaRlDT_us",
+  authDomain: "auth-firebase-context-ta-2c3e2.firebaseapp.com",
+  projectId: "auth-firebase-context-ta-2c3e2",
+  storageBucket: "auth-firebase-context-ta-2c3e2.appspot.com",
+  messagingSenderId: "22679708130",
+  appId: "1:22679708130:web:d2d61f1a1b4eb575bd99bb"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
